8599622c712e2a086b580f4d2add23973e28fc32
femioye <johnytrip4u@yahoo.com>
2018-02-27 19:06:59 -0600

    function js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-     'extends': 'airbnb-base' },
+     'extends': 'airbnb-base',
 'extends': 'airbnb-base',
 'rules': {
-    'no-plusplus': ['none']
+    'no-plusplus': ['0'],
+    'strict': [0],
+    'spce-before-function-paren': [0],
+    'func-names': [0],
 }
+
+};
---
+++ functions.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//expression
+const even = function(params){
+ const result = param % 2 === 0;
+ console.log (result);
+};
+
+even('some stuff');
